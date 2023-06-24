b638e129b6f83b0eeb8ded7f11b32de2fe88a34e
Max <grzannamax@gmail.com>
2023-06-24 15:39:34 +0200

    use glfs for csv files

--- api.py
+++ api.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee3207d5964f44199e003954cb4d88eb312c84760721156860ebd8e5032ba57f
-size 1845
+oid sha256:76757ee0ba94694208bc10431a9387664de079b605f38b2ad2d0a81dabb1ea0a
+size 1641
--- trend.py
+++ trend.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b02ed4464c5b4bb760c5ca8083c20f1b9bb3b608b9ab9355ff21703ac9712c0a
-size 991
+oid sha256:668ff18a0d1dc35985141d0db159a9443bbb5a49f80d675abe7de24130f1f976
+size 990
